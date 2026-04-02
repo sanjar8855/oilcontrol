0b29aa5f116997c1b659242371f7b5cc9bd355c9
sanjar <38586745+sanjar8855@users.noreply.github.com>
2026-04-02 22:43:41 +0500

    .

--- database/migrations/2026_04_01_165900_add_phone_and_make_email_nullable_to_users_table.php
+++ database/migrations/2026_04_01_165900_add_phone_and_make_email_nullable_to_users_table.php
@@ -12,7 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('phone')->nullable()->unique()->after('email');
+            if (!Schema::hasColumn('users', 'phone')) {
+                $table->string('phone')->nullable()->unique()->after('email');
+            }
             $table->string('email')->nullable()->change();
         });
     }
@@ -23,7 +25,9 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('phone');
+            if (Schema::hasColumn('users', 'phone')) {
+                $table->dropColumn('phone');
+            }
             $table->string('email')->nullable(false)->change();
         });
     }
