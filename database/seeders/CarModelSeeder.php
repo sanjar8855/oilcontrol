2fda5093baff07fd14f33d51d25e311fa7d27af0
sanjar_8855 <niggard98@mail.ru>
2026-08-03 15:31:52 +0500

    .

--- database/seeders/CarModelSeeder.php
+++ database/seeders/CarModelSeeder.php
@@ -8,26 +8,77 @@ use Illuminate\Database\Seeder;
 class CarModelSeeder extends Seeder
 {
     /**
-     * O'zbekistonda aktiv sotuvdagi avtomobil turlari (nomlari), markasi bo'yicha guruhlangan.
+     * O'zbekistonda aktiv sotuvdagi avtomobil turlari, markasi bo'yicha guruhlangan.
+     * Har bir tur uchun taxminiy texnik xizmat ko'rsatuv ma'lumotlari:
+     * [motor moyi hajmi (litr), antifriz min (litr), antifriz max (litr)].
+     * Bu qiymatlar odatiy servis kitobchalariga asoslangan taxminiy son bo'lib,
+     * "Avto markalari" sahifasida har bir marka/model uchun alohida tahrirlash mumkin.
      */
     private const CAR_MODELS_BY_MAKE = [
         'Chevrolet' => [
-            'Cobalt', 'Nexia 1', 'Nexia 2', 'Nexia 3', 'Malibu', 'Malibu 2', 'Spark', 'Matiz',
-            'Damas', 'Labo', 'Lacetti', 'Gentra', 'Onix', 'Tracker',
-            'Captiva', 'Monza', 'Equinox', 'TrailBlazer',
+            'Cobalt' => [3.2, 5.7, 6.0],
+            'Nexia 1' => [3.5, 6.0, 6.5],
+            'Nexia 2' => [3.8, 5.0, 7.0],
+            'Nexia 3' => [3.5, 6.5, 7.0],
+            'Malibu' => [4.7, 7.0, 7.5],
+            'Malibu 2' => [4.5, 7.5, 8.0],
+            'Spark' => [2.8, 5.0, 5.5],
+            'Matiz' => [2.8, 4.5, 5.0],
+            'Damas' => [2.8, 5.0, 5.5],
+            'Labo' => [2.8, 5.0, 5.5],
+            'Lacetti' => [3.8, 6.5, 7.0],
+            'Gentra' => [3.5, 6.5, 7.0],
+            'Onix' => [3.2, 5.7, 6.2],
+            'Tracker' => [4.2, 6.5, 7.0],
+            'Captiva' => [5.0, 7.5, 8.5],
+            'Monza' => [3.2, 6.0, 6.5],
+            'Equinox' => [4.7, 7.0, 7.5],
+            'TrailBlazer' => [4.7, 9.0, 10.0],
         ],
         'BYD' => [
-            'Song Plus', 'Song Plus Champion', 'Chazor', 'Yuan Up',
-            'Qin Plus', 'Han', 'Tang', 'Seal',
+            'Song Plus' => [4.0, 7.0, 8.0],
+            'Song Plus Champion' => [4.0, 7.0, 8.0],
+            'Chazor' => [4.0, 7.0, 8.0],
+            'Yuan Up' => [3.5, 6.0, 7.0],
+            'Qin Plus' => [3.7, 6.5, 7.0],
+            'Han' => [null, 8.0, 9.0],
+            'Tang' => [5.5, 9.0, 10.0],
+            'Seal' => [null, 9.0, 9.5],
         ],
         'Kia' => [
-            'K5', 'Sportage', 'Sorento', 'Seltos',
-            'Cerato (K3)', 'Carnival',
+            'K5' => [4.6, 7.0, 7.5],
+            'Sportage' => [4.6, 8.0, 8.5],
+            'Sorento' => [5.6, 9.0, 9.5],
+            'Seltos' => [4.3, 6.5, 7.0],
+            'Cerato (K3)' => [4.3, 6.5, 7.0],
+            'Carnival' => [5.6, 9.0, 9.5],
         ],
-        'Chery' => ['Tiggo 4', 'Tiggo 7 Pro', 'Tiggo 8 Pro', 'Arrizo 5', 'Arrizo 6'],
-        'Haval' => ['Jolion', 'M6', 'F7', 'H6'],
-        'Hyundai' => ['Elantra', 'Tucson', 'Santa Fe', 'Sonata', 'Accent'],
-        'Lada (VAZ)' => ['Granta', 'Vesta', 'Niva', 'XRAY'],
+        'Chery' => [
+            'Tiggo 4' => [3.2, 6.0, 6.5],
+            'Tiggo 7 Pro' => [4.5, 7.0, 7.5],
+            'Tiggo 8 Pro' => [4.7, 8.0, 8.5],
+            'Arrizo 5' => [3.5, 6.5, 7.0],
+            'Arrizo 6' => [4.0, 7.0, 7.5],
+        ],
+        'Haval' => [
+            'Jolion' => [4.3, 7.0, 7.5],
+            'M6' => [3.7, 6.5, 7.0],
+            'F7' => [4.5, 7.5, 8.0],
+            'H6' => [4.5, 7.5, 8.5],
+        ],
+        'Hyundai' => [
+            'Elantra' => [3.8, 6.4, 7.0],
+            'Tucson' => [4.3, 8.0, 9.0],
+            'Santa Fe' => [5.9, 9.0, 10.0],
+            'Sonata' => [4.5, 7.0, 7.5],
+            'Accent' => [3.3, 6.0, 6.5],
+        ],
+        'Lada (VAZ)' => [
+            'Granta' => [3.5, 7.4, 7.8],
+            'Vesta' => [3.5, 7.5, 8.0],
+            'Niva' => [3.8, 9.8, 10.5],
+            'XRAY' => [3.5, 7.5, 8.0],
+        ],
     ];
 
     /**
@@ -38,8 +89,15 @@ class CarModelSeeder extends Seeder
         foreach (self::CAR_MODELS_BY_MAKE as $makeName => $models) {
             $make = CarMake::firstOrCreate(['name' => $makeName]);
 
-            foreach ($models as $modelName) {
-                $make->carModels()->firstOrCreate(['name' => $modelName]);
+            foreach ($models as $modelName => [$oil, $antifreezeMin, $antifreezeMax]) {
+                $make->carModels()->updateOrCreate(
+                    ['name' => $modelName],
+                    [
+                        'oil_capacity_liters' => $oil,
+                        'antifreeze_capacity_min_liters' => $antifreezeMin,
+                        'antifreeze_capacity_max_liters' => $antifreezeMax,
+                    ]
+                );
             }
         }
     }
